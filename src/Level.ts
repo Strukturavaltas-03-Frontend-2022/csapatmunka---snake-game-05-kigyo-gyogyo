45b6ca15543e9a0d883a1a39c28cf67008b1f8fb
Zo <pahyzoard@outlook.hu>
2022-10-21 10:42:51 +0200

    Level.ts completed

--- src/Level.ts
+++ src/Level.ts
@@ -1,5 +1,5 @@
-import Piece from './Piece';
-import { SIZE } from './constants';
+import Piece from "./Piece";
+import { SIZE } from "./constants";
 
 type coord = [number, number];
 type line = coord[];
@@ -10,38 +10,43 @@ export default class Level {
 
   garden: HTMLDivElement;
 
-  constructor (private generatorFunction: (rows: number, cols: number) => LevelMap) {
+  constructor(
+    private generatorFunction: (rows: number, cols: number) => LevelMap
+  ) {
     this.generatorFunction = generatorFunction;
-    this.garden = (document.getElementById('garden') as HTMLDivElement);
+    this.garden = document.getElementById("garden") as HTMLDivElement;
   }
 
   /**
    * FELADAT!
    * Hozz létre egy metódust translate néven!
-   * Visszatérési érték: mind a két kapott paramétert kerekítsd lefelé, 
+   * Visszatérési érték: mind a két kapott paramétert kerekítsd lefelé,
    * majd szorozd meg egyenként a SIZE konstanssal.
    * @param x {number} - x koordináta
    * @param y {number} - y koordináta
    * @returns {coord} - egy [x, y] koordinátával tér vissza
    */
+  translate(x: number, y: number): coord {
+    return [Math.floor(x) * SIZE, Math.floor(y) * SIZE];
+  }
 
-  remove (): void {
-    this.pieces.forEach(piece => {
+  remove(): void {
+    this.pieces.forEach((piece) => {
       piece.remove();
     });
   }
 
-  line (x0: number, y0: number, x1: number, y1: number): void {
+  line(x0: number, y0: number, x1: number, y1: number): void {
     const dx = Math.abs(x1 - x0);
     const dy = Math.abs(y1 - y0);
-    const sx = (x0 < x1) ? 1 : -1;
-    const sy = (y0 < y1) ? 1 : -1;
+    const sx = x0 < x1 ? 1 : -1;
+    const sy = y0 < y1 ? 1 : -1;
     let err = dx - dy;
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
       const [tx0, ty0] = this.translate(x0, y0);
-      this.pieces.push(new Piece({ x: tx0, y: ty0, type: 'wall' }));
+      this.pieces.push(new Piece({ x: tx0, y: ty0, type: "wall" }));
 
       // break when line is done
       if (Math.abs(x0 - x1) <= 0.5 && Math.abs(y0 - y1) <= 0.5) break;
@@ -64,50 +69,42 @@ export default class Level {
    * render method
    * @returns {void}
    */
-  render (): void {
-    /** 
+  render(): void {
+    /**
      * FELADAT!
-     * @var {number} cols - this.garden.clientHeight és SIZE hányadosa, 
-     * lefelé kerekítve 
+     * @var {number} cols - this.garden.clientHeight és SIZE hányadosa,
+     * lefelé kerekítve
+     */ const cols: number = Math.floor(this.garden.clientHeight / SIZE);
+    /**
+     * FELADAT!
+     * @var {number} rows - this.garden.clientWidth és SIZE hányadosa,
+     * lefelé kerekítve
      */
-
-    
-    
-    /** 
+    const rows: number = Math.floor(this.garden.clientWidth / SIZE);
+    /**
      * FELADAT!
-     * @var {number} rows - this.garden.clientWidth és SIZE hányadosa, 
-     * lefelé kerekítve 
-     */
-
-    
-    
-    /** 
-     * FELADAT!
-     * @var {LevelMap} level - this.generatorFunction által visszaadott érték, 
+     * @var {LevelMap} level - this.generatorFunction által visszaadott érték,
      * a rows és cols paraméterekkel
      */
+    const level: LevelMap = this.generatorFunction(rows, cols);
 
-    
-
-    level.forEach(line => {
+    level.forEach((line) => {
       const [x0, y0]: coord = line[0];
 
       /**
        * FELADAT!
-       * Olvasd ki a fenti sorhoz hasonlóan az x1 és y1 koordinátákat is, 
+       * Olvasd ki a fenti sorhoz hasonlóan az x1 és y1 koordinátákat is,
        * a line második eleméből!
        */
 
-      
-
+      const [x1, y1]: coord = line[1];
       /**
        * FELADAT!
-       * Hívd meg a this.line metódust, és add neki át az x0, y0, x1, y1 
+       * Hívd meg a this.line metódust, és add neki át az x0, y0, x1, y1
        * értékeket.
+       *
        */
-
-      
-
+      this.line(x0, y0, x1, y1);
     });
   }
 }
